d6585bd09459bd8e8de18528f3824110a398ae7c
Luna Flávia Costa <lunaflavia79@gmail.com>
2024-09-05 23:46:59 +0100

    adding data

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log(dados);
+// console.log(dados);
+
+let section = document.getElementById("search-results");
+console.log(section);
+
+for (let data of data) {
+    section.innerHTML =
+        <div class="result-item">
+            <h2>=${data.title}</h2>
+            <p class="descricao-meta">${data.description}</p>
+        </div>
+}
---
+++ data.js
@@ -0,0 +1,38 @@
+let dados = [
+    {
+        title: "Trilho Azul de Monsanto",
+        type: "Park",
+        description: "Discover paradise with your furry friend on the Blue Trail of Monsanto! Nestled in the heart of Lisbon's Monsanto Forest Park, this trail offers stunning views and endless nature. At around 7.7 km, it's perfect for longer adventures with your dog, exploring the area's amazing biodiversity.",
+        link: "https://maps.app.goo.gl/UwipUz4JVsMsrmEr5"
+    },
+    {
+        title: "Albufeira do Rio da Mula",
+        type: "Lagoon",
+        description: "A beautiful lagoon within the Quinta do Pisão in Sintra-Cascais Natural Park. This pet-friendly spot offers stunning natural scenery, perfect for a relaxing day with your furry companion. Enjoy a walk around the lagoon, go for a swim (check for pet restrictions beforehand), or simply soak up the sun.",
+        link: "https://maps.app.goo.gl/cCRAyCgW2Pti5ufb8"
+    },
+    {
+        title: "Praia da Cresmina",
+        type: "Beach",
+        description: "A haven in the Cascais region for you and your pet to enjoy a day at the beach. With ample space, crystal clear waters, and stunning natural scenery, the beach offers everything you need to relax and have fun with your four-legged companion. In addition, the beach has complete infrastructure, including a restaurant, bar, and first aid station, ensuring comfort and safety.",
+        link: "https://maps.app.goo.gl/6RscwyrJ6cNTifnv9"
+    },
+    {
+        title: "Quinta da Alfarrobeira",
+        type: "Park",
+        description: "A green haven in the heart of Lisbon, this is the perfect place for you and your pet to enjoy some leisure time and get in touch with nature. Located in the São Domingos de Benfica region, this historic park offers green spaces that are ideal for walking and playing with your dog. Quinta da Alfarrobeira is a true urban oasis with a rich history and charming landscaping.",
+        link: "https://maps.app.goo.gl/RexdgYeYTgQ18Ugc8"
+    },
+    {
+        title: "Praia do Barranquinho",
+        type: "Beach",
+        description: "A paradise for you and your best friend, this is the first official dog beach in Portugal! Located in the Lagoa region of the Algarve, this extensive area of ​​around 80 meters offers a safe and exclusive space for your dog to have fun in the sea and on the sand.",
+        link: "https://maps.app.goo.gl/d4tSTvZqke9RsUg79"
+    },
+    {
+        title: "Praia Fonte da Telha",
+        type: "Beach",
+        description: "One of the postcards of Costa da Caparica, it is a destination that combines natural beauty, tranquility, and fun. With a vast stretch of sand, lush dunes, and a sea of ​​crystal clear waters, this beach offers the perfect setting to enjoy a sunny and relaxing day. Although it isn't an officially designated dog-friendly beach, Fonte da Telha has large areas where pets can play freely, as long as they are kept under supervision and hygiene rules are respected.",
+        link: "https://maps.app.goo.gl/x48zPPy9D8WVDPN68"
+    },
+];
