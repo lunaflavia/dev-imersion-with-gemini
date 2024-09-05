425b1e03c37417c47dc9d59d7ad89374331ce43b
Luna Flávia Costa <lunaflavia79@gmail.com>
2024-09-05 10:59:48 +0100

    add dados and app js

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        title : "Trilho Azul de Monsanto",
+        type : "Park",
+        description : "Discover paradise with your furry friend on the Blue Trail of Monsanto! Nestled in the heart of Lisbon's Monsanto Forest Park, this trail offers stunning views and endless nature. At around 7.7 km, it's perfect for longer adventures with your dog, exploring the area's amazing biodiversity.",
+        link : "https://maps.app.goo.gl/UwipUz4JVsMsrmEr5"
+    },
+    {
+        title : "Albufeira do Rio da Mula",
+        type : "Lagoon",
+        description : "A beautiful lagoon within the Quinta do Pisão in Sintra-Cascais Natural Park. This pet-friendly spot offers stunning natural scenery, perfect for a relaxing day with your furry companion. Enjoy a walk around the lagoon, go for a swim (check for pet restrictions beforehand), or simply soak up the sun.",
+        link : "https://maps.app.goo.gl/cCRAyCgW2Pti5ufb8"
+    },
+    {
+        title : "Praia da Cresmina",
+        type : "Beach",
+        description : "A haven in the Cascais region for you and your pet to enjoy a day at the beach. With ample space, crystal clear waters, and stunning natural scenery, the beach offers everything you need to relax and have fun with your four-legged companion. In addition, the beach has complete infrastructure, including a restaurant, bar, and first aid station, ensuring comfort and safety.",
+        link : "https://maps.app.goo.gl/6RscwyrJ6cNTifnv9"
+    },
+];
